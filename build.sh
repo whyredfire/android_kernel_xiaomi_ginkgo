761df6abf5cebd5c6985171047787e631f7736e9
DarkJoker360 <simoespo159@gmail.com>
2020-11-07 02:14:46 -0800

    [SQUASH] ginkgo: Drop dtbo support
    Revert "build.sh: Build and ship DTBO in our AK3 package"
    
    This reverts commit f310595ec1c293e93f6a887163964ecc64aa076a.
    
    Revert "scripts: use python rewrite in libfdt for mkdtimg"
    
    This reverts commit 146ea6bc68148f961c85a0481ce49320d4b867a5.
    
    Revert "dtbo.img: build device tree overlay partition image"
    
    This reverts commit 2b65b5dae1c7d5c431c5e4a9a09c0590c69a3d1a.

--- build.sh
+++ build.sh
@@ -20,14 +20,13 @@ echo -e "\nRegened defconfig succesfully!"
 exit 0
 else
 echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb dtbo.img
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb
 fi
 
-if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
-echo -e "\nKernel compiled succesfully! Zipping up...\n"
+if [ -f "out/arch/arm64/boot/Image.gz-dtb" ]; then
 git clone -q https://github.com/ghostrider-reborn/AnyKernel3
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
-cp out/arch/arm64/boot/dtbo.img AnyKernel3
+rm -rf out/arch/arm64/boot
 cd AnyKernel3
 zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
 cd ..
@@ -38,7 +37,7 @@ sshpass -sp $sfpass scp $ZIPNAME whyredfire@frs.sourceforge.net:/home/frs/projec
 else
 echo "Zip: $ZIPNAME"
 fi
-rm -rf out/arch/arm64/boot
+#rm -rf out
 else
 echo -e "\nCompilation failed!"
 fi
