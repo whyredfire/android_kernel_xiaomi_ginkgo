f5d867e01db2a3dee2b5ecaf15271d06210068de
whyredfire <whyredfire@gmail.com>
2021-05-31 18:00:50 +0000

    defconfig: regen for RR

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 export PATH="$HOME/proton/bin:$PATH"
 SECONDS=0
-ZIPNAME="QuicksilveR-ReloadedOS-Edition-$(date '+%Y%m%d-%H%M').zip"
+ZIPNAME="QuicksilveR-RR-$(date '+%Y%m%d-%H%M').zip"
 
 if ! [ -d "$HOME/proton" ]; then
 echo "Proton clang not found! Cloning..."
